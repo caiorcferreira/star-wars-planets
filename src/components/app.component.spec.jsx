1aca394c593f07af0517db51ecbbec6ea02c372c
Caio Ferreira <caiorcferreira@gmail.com>
2018-02-16 22:40:15 -0200

    Implement snapshot testing for application component.

---
+++ src/components/app.component.spec.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { App } from "./app.component";
+
+import { planetFixture } from "../tests/fixture/planet.fixture";
+
+describe("Application component", () => {
+  let fetchPlanet, planet;
+  beforeEach(() => {
+    fetchPlanet = jest.fn();
+    planet = {
+      data: planetFixture,
+      loading: false,
+      error: null
+    };
+  });
+
+  it("should render component with planet", () => {
+    const wrapper = shallow(<App fetchPlanet={fetchPlanet} planet={planet} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render component loading", () => {
+    const wrapper = shallow(
+      <App fetchPlanet={fetchPlanet} planet={{ ...planet, loading: true }} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
