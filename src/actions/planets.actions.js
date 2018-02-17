5b5fac598367dea2578c699a0679be6ce6612789
Caio Ferreira <caiorcferreira@gmail.com>
2018-02-17 14:44:44 -0200

    Update action generator to use extracted functions.

--- src/actions/planets.actions.js
+++ src/actions/planets.actions.js
@@ -2,43 +2,17 @@ export const FETCH_PLANET_BEGIN = "FETCH_PLANET_BEGIN";
 export const FETCH_PLANET_SUCCESS = "FETCH_PLANET_SUCCESS";
 export const FETCH_PLANET_FAILURE = "FETCH_PLANET_FAILURE";
 
-const getRandomInt = (min, max) => {
-  const minInt = Math.ceil(min);
-  const maxInt = Math.floor(max);
-  return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
-};
-
-const handleHtppErrors = response => {
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-
-  return response;
-};
+import { getRandomPlanet } from "../services/planetsService/planets.service";
 
 export const fetchPlanet = () => {
   return async dispatch => {
-    const randomId = getRandomInt(1, 61);
-
-    dispatch(fetchPlanetBegin());
     try {
-      const json = await fetch(`https://swapi.co/api/planets/${randomId}`)
-        .then(handleHtppErrors)
-        .then(response => response.json());
+      dispatch(fetchPlanetBegin());
+      const planet = await getRandomPlanet();
+      dispatch(fetchPlanetSuccess(planet));
     } catch (error) {
       dispatch(fetchPlanetFailure());
     }
-
-    const { name, population, climate, terrain, films } = json;
-    dispatch(
-      fetchPlanetSuccess({
-        name,
-        population,
-        climate,
-        terrain,
-        filmsFeatured: films.length
-      })
-    );
   };
 };
 
