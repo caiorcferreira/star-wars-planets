c540f4e9e600b50a0ed68facaf40b13acca93bf5
Caio Ferreira <caiorcferreira@gmail.com>
2018-02-16 23:04:29 -0200

    Implement error handling for API call

--- src/actions/planets.actions.js
+++ src/actions/planets.actions.js
@@ -1,5 +1,6 @@
 export const FETCH_PLANET_BEGIN = "FETCH_PLANET_BEGIN";
 export const FETCH_PLANET_SUCCESS = "FETCH_PLANET_SUCCESS";
+export const FETCH_PLANET_FAILURE = "FETCH_PLANET_FAILURE";
 
 const getRandomInt = (min, max) => {
   const minInt = Math.ceil(min);
@@ -7,14 +8,26 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
 };
 
+const handleHtppErrors = response => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+};
+
 export const fetchPlanet = () => {
   return async dispatch => {
     const randomId = getRandomInt(1, 61);
 
     dispatch(fetchPlanetBegin());
-    const json = await fetch(`https://swapi.co/api/planets/${randomId}`).then(
-      response => response.json()
-    );
+    try {
+      const json = await fetch(`https://swapi.co/api/planets/${randomId}`)
+        .then(handleHtppErrors)
+        .then(response => response.json());
+    } catch (error) {
+      dispatch(fetchPlanetFailure());
+    }
 
     const { name, population, climate, terrain, films } = json;
     dispatch(
@@ -36,4 +49,8 @@ export const fetchPlanetSuccess = planet => ({
 
 export const fetchPlanetBegin = () => ({
   type: FETCH_PLANET_BEGIN
+});
+
+export const fetchPlanetFailure = () => ({
+  type: FETCH_PLANET_SUCCESS
 });
