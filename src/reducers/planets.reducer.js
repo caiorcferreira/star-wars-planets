94373d8243cb5d6937db55ff6ce66b36bdb34348
Caio Ferreira <caiorcferreira@gmail.com>
2018-02-17 15:43:58 -0200

    Update reducer to handle error state.

--- src/reducers/planets.reducer.js
+++ src/reducers/planets.reducer.js
@@ -1,6 +1,7 @@
 import {
+  FETCH_PLANET_BEGIN,
   FETCH_PLANET_SUCCESS,
-  FETCH_PLANET_BEGIN
+  FETCH_PLANET_FAILURE
 } from "../actions/planets.actions";
 
 const initialState = {
@@ -17,6 +18,11 @@ export default (state = initialState, action) => {
     case FETCH_PLANET_SUCCESS:
       const { planet } = action.payload;
       return { ...state, data: planet, loading: false, error: null };
+      break;
+    case FETCH_PLANET_FAILURE:
+      const error =
+        "Sorry, the force is disturbed and we couldn't get the planets data";
+      return { ...state, data: {}, loading: false, error };
       break;
     default:
       return state;
